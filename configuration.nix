c03517509064f660a552dec6180e9e73ce9e2276
Paul Puscasu <paul.g.puscasu@gmail.com>
2024-01-22 08:31:35 +0000

    [DUAL-BOOT] Fix DualBoot and some configs for NixOs

--- configuration.nix
+++ configuration.nix
@@ -10,34 +10,58 @@
       ./hardware-configuration.nix
     ];
 
-  # Use the GRUB 2 boot loader.
-  boot.loader.grub.enable = true;
-  # boot.loader.grub.efiSupport = true;
-  # boot.loader.grub.efiInstallAsRemovable = true;
-  # boot.loader.efi.efiSysMountPoint = "/boot/efi";
-  # Define on which hard drive you want to install Grub.
-  boot.loader.grub.device = "/dev/vda"; # or "nodev" for efi only
-  boot.kernelModules = [ "kvm-amd" ];
-  virtualisation.libvirtd.enable = true;
-  virtualisation.docker.enable = true;
-
-  services.flatpak.enable = true;
-  services.sshd.enable = true;
-  services.openssh.enable = true;
-
-  xdg.portal = {
-    enable = true;
-    extraPortals = with pkgs; [
-      xdg-desktop-portal-gtk
-    ];
+  # Use the systemd-boot EFI boot loader.
+  boot = {
+    tmp.cleanOnBoot = true;
+    supportedFilesystems = [ "ntfs" ];
+    kernelPackages = pkgs.linuxPackages_latest;
+    kernelModules = [ "admgpu" ];
+    loader = {
+      #systemd-boot.enable = true;
+      grub.configurationLimit = 5;
+      grub.enable = true;
+      grub.efiSupport = true;
+      grub.efiInstallAsRemovable = true;
+      grub.device = "nodev";
+      grub.extraEntriesBeforeNixOS = true;
+      grub.useOSProber = true;
+      efi = {
+        canTouchEfiVariables = false;
+        efiSysMountPoint = "/boot";
+      };
+    };
   };
 
-  programs.dconf.enable = true;
+  #boot.kernelModules = [ "admgpu" ];
+  #boot.kernelPackages = pkgs.linuxPackages_latest;
+  #boot.supportedFilesystems = [ "ntfs" ];
+
+  virtualisation.libvirtd.enable = true;
+  virtualisation.libvirtd.qemu.swtpm.enable = true;
+  virtualisation.docker.enable = true;
+  
+  networking.hostName = "nixos"; # Define your hostname.
+
+  # Services
+  services.flatpak.enable = true;
+  #services.emacs.enable = true;
+
+  # Services Mouse
+  services.ratbagd.enable = true;
+ 
+  # Enable sound.
+  sound.enable = true;
+  # hardware.pulseaudio.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    pulse.enable = true;
+  };
 
   # networking.hostName = "nixos"; # Define your hostname.
   # Pick only one of the below networking options.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-  # networking.networkmanager.enable = true;  # Easiest to use and most distros use this by default.
+  networking.networkmanager.enable = true;  # Easiest to use and most distros use this by default.
 
   # Set your time zone.
   time.timeZone = "Europe/Amsterdam";
@@ -47,40 +71,71 @@
   # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
 
   # Select internationalisation properties.
-  # i18n.defaultLocale = "en_US.UTF-8";
+  i18n.defaultLocale = "en_US.UTF-8";
   console = {
-     font = "Lat2-Terminus16";
-     keyMap = "de";
-     #useXkbConfig = true; # use xkbOptions in tty.
+    font = "Lat2-Terminus16";
+    keyMap = "de";
+  #   useXkbConfig = true; # use xkbOptions in tty.
   };
 
   # Enable the X11 windowing system.
-  services.xserver.enable = true;
+  # services.xserver.enable = true;
 
-  #services.xserver.displayManager.sddm.enable = true;
-  services.xserver.windowManager.i3.enable = true;
+  # Enable Hyprland
+  programs.hyprland.enable = true;
+  programs.waybar.enable = true;
+  programs.hyprland.xwayland.enable = true;
+  environment.sessionVariables.NIXOS_OZONE_WL = "1";    
+  
+  # Steam gaming
+  programs.steam.enable = true;
 
-  ## Configure keymap in X11
-  services.xserver.layout = "de";
+  xdg.portal = {
+    enable = true;
+    wlr.enable = true;
+    # gtk portal needed to make gtk apps happy
+    extraPortals = [ pkgs.xdg-desktop-portal-gtk ];
+  };
+
+  # Configure keymap in X11
+  # services.xserver.layout = "us";
   # services.xserver.xkbOptions = "eurosign:e,caps:escape";
 
   # Enable CUPS to print documents.
   # services.printing.enable = true;
 
-  # Enable sound.
-   sound.enable = true;
-   hardware.pulseaudio.enable = true;
 
   # Enable touchpad support (enabled default in most desktopManager).
   # services.xserver.libinput.enable = true;
 
   # Define a user account. Don't forget to set a password with ‘passwd’.
+  nixpkgs.config.allowUnfree = true;
   users.users.wally = {
-     isNormalUser = true;
-     extraGroups = [ "wheel" "libvirtd" "storage" "docker" ]; # Enable ‘sudo’ for the user.
-     packages = with pkgs; [
-       vim
-     ];
+    isNormalUser = true;
+    extraGroups = [ "wheel" "networkmanager" "storage" "libvirtd" "docker" "input" "disk" "kvm" ]; # Enable ‘sudo’ for the user.
+    packages = with pkgs; [
+      starship
+      lolcat
+      distrobox
+      k3d
+      htop
+      libnotify
+      networkmanagerapplet
+      python3
+      python311Packages.pip
+      shellcheck
+      evince
+      viewnior
+      # Rust cli tools
+      ripgrep
+      du-dust
+      procs
+      tealdeer
+      eww-wayland
+      spotify
+      zoxide
+      popcorntime
+    ];
   };
 
   # List packages installed in system profile. To search, run:
@@ -89,20 +144,140 @@
     vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     wget
     curl
-    git
-    alacritty
-    distrobox
-    virt-manager
+    gnugrep
     neofetch
+    macchina
+    vlc
+    starship
+    lolcat
+    alacritty
+    virt-manager
+    virt-viewer
+    virtio-win
+    #pcmanfm
+    dunst
+    rofi
+    wofi
+    waybar
+    git
+    curl
+    eza
+    ansible
+    bat
+    bc
+    jq
+    lm_sensors
+    gparted
+    nerdfonts
+    pciutils
+    hyprland
+    hyprpaper
+    hyprland-protocols    
+    wlr-randr
+    k3d
+    alsa-utils    
+    hashcat
+    hashcat-utils
+    stress-ng    
+    #brave
+    glib
+    # Themes
+    #dracula-theme
+    #dracula-icon-theme
+    #catppuccin-cursors
+    #lxappearance-gtk2
+    #libsForQt5.qtstyleplugin-kvantum
+    # Emacs dependency
+    #emacs29
+    #fd
+    #libgcc
+    #cmake
+    #gnumake
+    # Bluetooth
+    bluez
+    blueberry
+    unzip
+    # Rust + Programs
+    rustc
+    cargo
+    rustup
+    bottom
+    # Pkill
+    killall
+    # themes
+    beauty-line-icon-theme
+    dracula-theme
+    polkit_gnome
+    os-prober
+    exfat
+    exfatprogs
+    fastfetch
+    grim
+    slurp
+    efibootmgr
   ];
+
+  environment.localBinInPath = true;
+
+  environment.variables = {
+    #"QT_STYLE_OVERRIDE" = "kvantum";
+    "QT_QPA_PLATFORMTHEME" = "gtk2";
+    "GTK_THEME" = "Dracula";
+    "ICON_THEME" = "BeautyLine";
+  };
+
+  services.dbus.enable = true;
+
+  security.polkit.enable = true;
+  systemd = {
+  user.services.polkit-gnome-authentication-agent-1 = {
+    description = "polkit-gnome-authentication-agent-1";
+    wantedBy = [ "graphical-session.target" ];
+    wants = [ "graphical-session.target" ];
+    after = [ "graphical-session.target" ];
+    serviceConfig = {
+        Type = "simple";
+        ExecStart = "${pkgs.polkit_gnome}/libexec/polkit-gnome-authentication-agent-1";
+        Restart = "on-failure";
+        RestartSec = 1;
+        TimeoutStopSec = 10;
+      };
+  };
+};
+
+  fonts = {
+    packages = with pkgs; [
+      font-awesome
+      source-code-pro
+      (nerdfonts.override { fonts = [ "SourceCodePro" ]; })
+    ];
+    fontconfig = {
+      enable = true;
+      defaultFonts = {
+	      monospace = [ "Meslo LG M Regular Nerd Font Complete Mono" ];
+	      serif = [ "Noto Serif" "Source Han Serif" ];
+	      sansSerif = [ "Noto Sans" "Source Han Sans" ];
+      };
+    };
+  }; 
+
+  # Environment etc
+  environment.etc = {
+    "gtk-3.0".source = /home/wally/.config/gtk-3.0;
+    "gtk-2.0".source = /home/wally/.config/gtk-2.0;
+  };
+
+  # qt Theme
+  #qt.enable = true;
+  #qt.platformTheme = "gtk2";
 
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
+  programs.gnupg.agent = {
+    enable = true;
   #   enableSSHSupport = true;
-  # };
+  };
 
   # List services that you want to enable:
 
@@ -118,7 +293,7 @@
   # Copy the NixOS configuration file and link it from the resulting system
   # (/run/current-system/configuration.nix). This is useful in case you
   # accidentally delete configuration.nix.
-  # system.copySystemConfiguration = true;
+  system.copySystemConfiguration = true;
 
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions

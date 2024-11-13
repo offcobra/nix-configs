5ef47aaa45e5f688c1966765907791f73e80cdf1
Paul Puscasu <paul-george.puscasu@fcbayern.com>
2024-11-13 13:20:02 +0100

    Moved hypridle & hyprlock to nix declarative lang from .text...

--- user/wayland/hypridle.nix
+++ user/wayland/hypridle.nix
@@ -1,41 +1,45 @@
-{ ... }:
+{ systemSettings, ... }:
 
+let
+  timer = if (systemSettings.hostname == "workstation")
+          # On Workstation PC...
+          then
+            [{
+              timeout = 300;                                 # 5 min
+              on-timeout = "loginctl lock-session";          # lock screen
+            } {
+              timeout = 600;                                 # 10 min
+              on-timeout = "screen-chill.sh";                # screen-chill script (2screens)
+              on-resume = "screen-full.sh";                  # screen-full
+            } {
+              timeout = 1200;                                # 20 min
+              on-timeout = "screen-work.sh";                 # screen-work script (1screens)
+              on-resume = "screen-full.sh";                  # screen-full
+            } {
+              timeout = 1800;                                # 30 min
+              on-timeout = "poweroff";                       # poweroff
+            }]
+          # Everywhere else...
+          else
+            [{
+              timeout = 300;                                 # 5 min
+              on-timeout = "loginctl lock-session";          # lock screen
+            } {
+              timeout = 1800;                                # 30 min
+              on-timeout = "poweroff";                       # poweroff
+            }];
+in
 {
   # Hypridle Configs
-  home.file = {
-    "hypridle.conf" = {
-      enable = true;
-      target = ".config/hypr/hypridle.conf";
-      text = "# OffTheWall's Hypridle Config
-general {
-    lock_cmd = pidof hyprlock || hyprlock       # launch hyprlock
-    before_sleep_cmd = loginctl lock-session    # lock before suspend.
-    after_sleep_cmd = hyprctl dispatch dpms on  # to avoid having to press a key twice to turn on the display.
-}
-
-listener {
-    timeout = 300                               # 5 min
-    on-timeout = loginctl lock-session          # lock screen
-}
-
-#listener {
-#    timeout = 600                               # 10 min
-#    on-timeout = screen_chill                   # screen_chill
-#    on-resume = screen_full                     # screen_full
-#}
-#
-#listener {
-#    timeout = 1200                              # 20 min
-#    on-timeout = screen_work                    # screen_work
-#    on-resume = screen_full                     # screen_full
-#}
-
-listener {
-    timeout = 1800                              # 30 min
-    on-timeout = poweroff                       # poweroff
-}
-      ";
+  services.hypridle = {
+    enable = true;
+    settings= {
+      general = {
+        lock_cmd = "pidof hyprlock || hyprlock";       # launch hyprlock
+        before_sleep_cmd = "loginctl lock-session";    # lock before suspend.
+        after_sleep_cmd = "hyprctl dispatch dpms on";  # to avoid having to press a key twice to turn on the display.
+      };
+      listener = timer;
     };
   };
 }
-
